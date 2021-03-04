7997bbbb3225e7c97934af18a22ec7a6f32ed2c4
Antonio Mikulić <25779038+antonio-mikulic@users.noreply.github.com>
2021-03-04 20:59:44 +0100

    Init

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<header>
+		<h1>Horticulture Greenwhite</h1>
+		<nav>
+			<ul>
+			  <li><a href="index.html">Home</a></li>
+			  <li><a href="rukomet.html">Landscapes</a></li>
+			  <li><a href="vaterpolo.html">People</a></li>
+			  <li><a href="plivanje.html">Book us</a></li>
+			  <li><a href="plivanje.html">About us</a></li>
+			</ul>
+		</nav>
+	</header>
---
+++ people.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Horticulture Greenwhite</title>
+</head>
+<body>
+
+<header>
+	<div id="logo"><h1>Horticulture Greenwhite</h1></div>
+	<nav>
+		<ul>
+		  <li><a href="aboutus.php">About us</a></li>
+		  <li><a href="bookus.php">Book us</a></li>
+		  <li><a href="people.php">People</a></li>
+		  <li><a href="landscapes.php">Landscapes</a></li>
+		  <li><a href="index.php">Home</a></li>
+		<div class="clear"></div>
+		</ul>
+	</nav>
+</header>
+	
+
+	<main id="wrapper">
+		<article>
+			<h1>Welcome to the official page of Horticulture Greenwhite</h1>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+		</article>
+	</main>
+	<footer><p>Web stranicu napravili: Dario Kos i Antonio Mikulić</p></footer>
+</body>
+</html>
